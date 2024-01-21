48f6b398697957b081a4f8af87abbb19e9db739e
Dimuth Kurukula <dimuthkurukula@gmail.com>
2024-01-21 10:35:08 -0500

    fix missing dependency

--- packages/hardhat/contracts/GhoLiquidator.sol
+++ packages/hardhat/contracts/GhoLiquidator.sol
@@ -10,17 +10,16 @@ contract GhoLiquidator {
     IGhoFlashMinter private ghoFlashMinter;
     address public gholiquidationReceiver;
 
-    // Mainnet GHO_TOKEN = 0x40D16FC0246aD3160Ccc09B8D0D3A2cD28aE6C2f;
-    // Mainnet GhoFlashMinter = 0xb639D208Bcf0589D54FaC24E655C79EC529762B8;
     constructor(address _aavePoolAddress, address _ghoFlashMintAddress, address _gholiquidationReceiver) {
         aavePoolAddress = _aavePoolAddress;
         ghoFlashMinter = IGhoFlashMinter(_ghoFlashMintAddress);
         gholiquidationReceiver = _gholiquidationReceiver;
     }
 
-     //TODO find address user contract on AavePoolV3 to liquidate
-     //TODO find address collatoralToken to liquidate
-     //TODO find find unit amount to liquidate
+     // NOTE: future state
+     // find address user contract on AavePoolV3 to liquidate
+     // find address collatoralToken to liquidate
+     // find find unit amount to liquidate
 
     function mintAndLiquidate(address _user, address _collatoralToken, uint _amount) public {
         bytes memory data = abi.encode(LiquidationData(_user, _collatoralToken, _amount));
